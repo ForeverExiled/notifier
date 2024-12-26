7a78ce9297058a1480958f5055495c91dbba513b
foreverexiled <foreverexiled23@gmail.com>
2024-12-26 21:46:08 +0500

    project restructurization & .gitignore update

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"]."/constants.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/init.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/core/constants.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/core/init.php";
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +9,7 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/init.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🦋Напоминалка🦋</title>
-    <link rel="manifest" href="/assets/site.webmanifest">
-    <link rel="stylesheet" href="/assets/style.css">
-    <link rel="apple-touch-icon" href="/assets/images/apple-touch-icon.png">
-    <link rel="icon" href="/assets/images/favicon-16x16.png" type="image/x-icon">
-    <link rel="icon" href="/assets/images/favicon-32x32.png" type="image/x-icon">
+    <link rel="stylesheet" href="/style.css">
     <script src="/script.js"></script>
 </head>
 <body>
