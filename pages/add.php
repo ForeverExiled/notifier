60960c0f1c15858765e4b7991089f8ecc50c3a78
foreverexiled <foreverexiled23@gmail.com>
2025-01-07 22:47:18 +0500

    fixed indentation & made textarea required

--- pages/add.php
+++ pages/add.php
@@ -3,10 +3,10 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/header.php";
 ?>
 
 <form id="form_create" action="/core/handler.php" method="post" onsubmit="validate(event)" novalidate>
-  <input type="hidden" name="action" value="create">
-  <textarea rows="10" cols="30" name="text" class="pacifico-regular"></textarea>
-  <input type="datetime-local" name="datetime" required>
-  <button type="submit" class="pacifico-regular">Добавить!</button>
+	<input type="hidden" name="action" value="create">
+	<textarea rows="10" cols="30" name="text" class="pacifico-regular" required></textarea>
+	<input type="datetime-local" name="datetime" required>
+	<button type="submit" class="pacifico-regular">Добавить!</button>
 </form>
 
 <?php require_once $_SERVER["DOCUMENT_ROOT"]."/footer.php";
